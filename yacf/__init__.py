eb16b080a4a40ca73ccc6226acda58f88c125a66
Max Resing <max.resing@protonmail.com>
2020-11-06 18:30:57 +0100

    Added usage to __init__

--- yacf/__init__.py
+++ yacf/__init__.py
@@ -12,12 +12,52 @@ and update them.
 
 [//] # "TODO: usage"
 
+The usage of this framework is straight-forward.
 
-## Features
+Create a Configuration instance. When creating one, give the inputs to read
+from as arguments. The input can be either a dictionary, to be immediately 
+used, or alternatively a file name which ends on one of the supported file
+types below.
 
-Supported file types:
+Afterwards, call the `load` function on the configuration instance. This
+function allows to define additional inputs. It's a matter of choice whether
+one wants to define the inputs in the constructor or in the `load` call.
 
-* ini
+The `load` function uses the previously defined configuration inputs. The 
+function builds one large configuration dictionary out of all the inputs.
+Overlapping parameters are always overwritten.
+
+This makes it easy to define configurations of different priorities. One good
+approach is to define the configuration inputs as follows:
+
+```
+[defaults, custom configuration, environment variables, command line arguments]
+```
+
+To access the values of the configuration, one can either use the regular
+access of dictionaries, e.g. a concatenation of gets(). Alternatively one can
+simply use dot notation:
+
+```
+config = Configuration('api-config.json').load()
+sample = config.get('api').get('hostname')
+
+# Alternative dot notation
+sample = config.get('api.hostname')
+```
+
+
+## Additional Features
+
+### Custom Seperator
+
+If you, for some reason dislike the regular seperator '.' in the dot notation
+you can choose a custom one when initializing the configuration instance.
+
+
+
+## Supported File Types
+
 * json
 * toml
 
